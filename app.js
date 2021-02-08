ab2e85a761973a5ec2b8da419775578fef3b90a8
Tazulislam <butterflybamfire12@gmail.com>
2021-02-08 19:59:51 +0600

    some code

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const searchMeal = () =>{
     const searchName = document.getElementById('inputValue').value;
    
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchName}`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`;
 
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
-
-    
-
+    .then(data => displayMeal(data.meals))
 }
 
- const handleSearch = document.getElementById('button').addEventListener('click', searchMeal);
+ const handleSearch = document.getElementById("button").addEventListener('click', searchMeal());
+
+ const displayMeal = (meal) =>{
+    //  console.log(meal);
+     const mealDiv = document.getElementById('mealItem');
+     
+        
+   
+ } 
+
+ 

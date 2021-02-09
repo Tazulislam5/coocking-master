56221751cc0f4e997e5d2d35f505cbf65f2e6618
Tazulislam <butterflybamfire12@gmail.com>
2021-02-09 11:34:55 +0600

    some code

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ const searchMeal = () =>{
 const displayMeal = meal =>{
     const mealContainer = document.getElementById('mealContainer');
   meal.forEach(mealItem => {
-    //    console.log(mealItem);
+      // console.log(mealItem);
      const mealDiv = document.createElement('div');
      mealDiv.className = 'meal';
      mealDiv.innerHTML = `
      <img src="${mealItem.strMealThumb}">
+     <button onclick = "displayIngredients(${mealItem.idMeal})">Details</button>
      `
      mealContainer.appendChild(mealDiv);
 
@@ -25,11 +26,34 @@ const displayMeal = meal =>{
  
 
 displayIngredients = id =>{
-  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${id}`
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
   fetch(url)
-  .then(res = res.json())
-  .then(data => console.log(data))
+  .then(res => res.json())
+  .then(data => mealInfo(data.meals[0]))
 }
+
+const mealInfo = meal =>{
+  // console.log(meal);
+  const foodDetails = document.getElementById('foodDetail');
+
+ 
+
+    
+  foodDetails.innerHTML= `<h2>Ingredients:</h2>
+  
+  <h3>.${meal.strIngredient1}</h3>
+  <h3>.${meal.strIngredient2}</h3>
+  <h3>.${meal.strIngredient3}</h3>
+  <h3>.${meal.strIngredient4}</h3>
+  <h3>.${meal.strIngredient5}</h3>
+  <h3>.${meal.strIngredient6}</h3>
+  <h3>.${meal.strIngredient7}</h3>
+  <h3>.${meal.strIngredient8}</h3>
+  `
+  
+  
+}
+
 
 
 
